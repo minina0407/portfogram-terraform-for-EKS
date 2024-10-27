cc4f678b0d85cd14eb486bd00482677758812662
minina <minina1868@gmail.com>
2024-10-28 01:39:47 +0900

    [fix] 변수 관련 발생 오류 수정

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,45 +1,45 @@
 variable "name_prefix" {
-  description = "리소스 이름 접두사"
+  description = "Prefix for resource names"
   type        = string
 }
 
 variable "cluster_name" {
-  description = "EKS 클러스터 이름"
+  description = "Name of the EKS cluster"
   type        = string
 }
 
 variable "kubernetes_version" {
-  description = "쿠버네티스 버전"
+  description = "Kubernetes version for the EKS cluster"
   type        = string
 }
 
 variable "vpc_id" {
-  description = "VPC ID"
+  description = "ID of the VPC where resources will be created"
   type        = string
 }
 
 variable "subnet_ids" {
-  description = "서브넷 ID 리스트"
+  description = "List of subnet IDs for EKS cluster"
   type        = list(string)
 }
 
 variable "eks_cluster_role_arn" {
-  description = "EKS 클러스터 IAM 역할 ARN"
+  description = "ARN of the IAM role for EKS cluster"
   type        = string
 }
 
 variable "eks_node_role_arn" {
-  description = "EKS 노드 IAM 역할 ARN"
+  description = "ARN of the IAM role for EKS nodes"
   type        = string
 }
 
 variable "eks_cluster_security_group_id" {
-  description = "EKS 클러스터 보안 그룹 ID"
+  description = "ID of the security group for EKS cluster"
   type        = string
 }
 
 variable "node_groups" {
-  description = "노드 그룹 설정"
+  description = "Configuration for EKS node groups"
   type = map(object({
     desired_size  = number
     max_size      = number
@@ -50,57 +50,57 @@ variable "node_groups" {
 }
 
 variable "tags" {
-  description = "태그 맵"
+  description = "Map of tags to apply to resources"
   type        = map(string)
   default     = {}
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR 블록"
+  description = "CIDR block for VPC"
   type        = string
 
   validation {
     condition     = can(cidrhost(var.vpc_cidr, 0))
-    error_message = "유효한 CIDR 블록을 입력하세요."
+    error_message = "Please provide a valid CIDR block format."
   }
 }
 
 variable "public_subnets_cidr" {
-  description = "퍼블릭 서브넷 CIDR 블록 리스트"
+  description = "List of CIDR blocks for public subnets"
   type        = list(string)
 
   validation {
     condition     = length(var.public_subnets_cidr) > 0
-    error_message = "최소 하나 이상의 퍼블릭 서브넷이 필요합니다."
+    error_message = "At least one public subnet CIDR block is required."
   }
 }
 
 variable "private_subnets_cidr" {
-  description = "프라이빗 서브넷 CIDR 블록 리스트"
+  description = "List of CIDR blocks for private subnets"
   type        = list(string)
 
   validation {
     condition     = length(var.private_subnets_cidr) > 0
-    error_message = "최소 하나 이상의 프라이빗 서브넷이 필요합니다."
+    error_message = "At least one private subnet CIDR block is required."
   }
 }
 
 variable "availability_zones" {
-  description = "가용영역 리스트"
+  description = "List of availability zones"
   type        = list(string)
 
   validation {
     condition     = length(var.availability_zones) >= 2
-    error_message = "고가용성을 위해 최소 2개 이상의 가용영역이 필요합니다."
+    error_message = "At least two availability zones are required for high availability."
   }
 }
 
 variable "private_subnet_ids" {
-    description = "프라이빗 서브넷 ID 리스트"
-    type        = list(string)
+  description = "List of private subnet IDs"
+  type        = list(string)
 }
 
 variable "public_subnet_ids" {
-    description = "퍼블릭 서브넷 ID 리스트"
-    type        = list(string)
+  description = "List of public subnet IDs"
+  type        = list(string)
 }
